99a13b67647ff85e6449138a4ebdcd690ba1eb69
二次蓝 <ercilan@163.com>
2020-11-30 16:24:30 +0800

    测试

--- src/script/marvin.nav2.js
+++ src/script/marvin.nav2.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
     $('#' + f + '>ul').html(j);
     b.data('spy', 'scroll');
     b.data('target', '.sideCatalogBg');
-    p && b.data('offset', '55');
+    p && b.data('offset', '100');
     b.scrollspy({
         target: '.sideCatalogBg'
     });
